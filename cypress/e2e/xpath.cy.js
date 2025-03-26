0e81349d8c867b39f283daa1063d034a63fe83e7
Chrispus Thiaine <chrispus.thiaine@mgas.ke>
2025-03-26 16:38:55 +0300

    comment change

--- cypress/e2e/xpath.cy.js
+++ cypress/e2e/xpath.cy.js
@@ -3,6 +3,7 @@ describe("word",()=>{
    it("visit",()=>{
     cy.baseURL();
     cy.xpath('//div[@class="_3sdu8W emupdz"]//div[@aria-label="Home & Furniture"]//div[@class="YBLJE4"]//span[contains(text(),"Home & Furniture")]').trigger("mouseover")
-    furniture('//div[@class="_16rZTH"]//object//a[text()="Furniture"]')
+    furniture('//div[@class="_16rZTH"]//object//a[text()="Furniture"]');
+    //adding a comment
  }) 
 })
